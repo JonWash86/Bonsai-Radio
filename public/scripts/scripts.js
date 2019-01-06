64f739cdb3d435e28ea169a4971fdca042da8378
Jon Washington <jonathan.washington@gmail.com>
2019-01-06 13:02:44 -0800

    stopping here, managed to add a click listener to my list items but still need to pass the id ofthe corresponding playlist

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -5,7 +5,7 @@ document.getElementById('playlistButton').addEventListener('click', function() {
 });
 
 function getTopTracks(access_token) {
-  console.log('getting tracks')
+  console.log('getting playlists')
   $.ajax({
     url: 'https://api.spotify.com/v1/me/playlists',
     headers: {
@@ -20,6 +20,29 @@ function getTopTracks(access_token) {
   });
 }
 
-function mapOverPlaylists(){
-  
+function mapOverPlaylists(playlists){
+  playlists.map(function(playlist){
+    var list = "<li id=" + playlist.id + " class='playlistItem'> <img src='" + playlist.images[0].url + "' height='75px'>" + playlist.name + "</li>"
+    document.getElementById('playlistList').innerHTML += list;
+  })
+  $('.playlistItem').click(function() {
+    var access_token = localStorage.getItem("access_token");
+    getPlaylistTracks(access_token);
+  })
+}
+
+function getPlaylistTracks(access_token){
+  console.log('getting tracks' + this.id)
+  $.ajax({
+    url: 'https://api.spotify.com/v1/me/playlists',
+    headers: {
+      'Authorization':'Bearer ' + access_token
+    },
+    success: function(response) {
+      console.log('hi!')
+      console.log(response.items);
+      $(".recommendations").show();
+      mapOverPlaylists(response.items);
+    }
+  });
 }
