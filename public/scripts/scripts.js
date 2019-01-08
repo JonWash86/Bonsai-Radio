6b2f7eb4335bc277e3c5074b485a40fd971eac1a
Jon Washington <jonathan.washington@gmail.com>
2019-01-07 16:56:52 -0800

    changed the playlist list into a dropdown menu for cleanliness. Selecting a playlist now automatically loads the songs on the lefthand side. Need to clean up css and add a line to clear the song list when a new query is made.

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,40 +1,41 @@
 
 document.getElementById('playlistButton').addEventListener('click', function() {
   var access_token = localStorage.getItem("access_token")
-  getTopTracks(access_token);
+  getPlaylists(access_token);
 });
 
-function getTopTracks(access_token) {
-  console.log('getting playlists')
+//This function gets a list of the user's playlists
+function getPlaylists(access_token) {
   $.ajax({
     url: 'https://api.spotify.com/v1/me/playlists',
     headers: {
       'Authorization':'Bearer ' + access_token
     },
     success: function(response) {
-      console.log('hi!')
-      console.log(response.items);
       $(".recommendations").show();
       mapOverPlaylists(response.items);
     }
   });
 }
 
+
 function mapOverPlaylists(playlists){
   playlists.map(function(playlist){
-    var list = "<li id=" + playlist.id + " class='playlistItem'> <img src='" + playlist.images[0].url + "' height='75px'>" + playlist.name + "</li>"
+    var list = "<option value=" + playlist.id + " class='playlistItem'> <img src='" + playlist.images[0].url + "' height='75px'>" + playlist.name + "</option>"
     document.getElementById('playlistList').innerHTML += list;
   })
-  $('.playlistItem').click(function() {
+  $('#playlistList').on('change', function() {
+    console.log('you selected a playlist!')
     var access_token = localStorage.getItem("access_token");
     getPlaylistTracks(access_token);
   })
 }
 
+//This function will use the access token to retrieve a list of the songs in a given playlist.
 function getPlaylistTracks(access_token){
-  console.log('getting tracks' + this.id)
+  console.log('getting tracks' + $('select option:selected').val())
   $.ajax({
-    url: 'https://api.spotify.com/v1/me/playlists',
+    url: 'https://api.spotify.com/v1/playlists/' + $('select option:selected').val() + '/tracks',
     headers: {
       'Authorization':'Bearer ' + access_token
     },
@@ -42,7 +43,18 @@ function getPlaylistTracks(access_token){
       console.log('hi!')
       console.log(response.items);
       $(".recommendations").show();
-      mapOverPlaylists(response.items);
+      mapOverTracks(response.items);
     }
   });
+}
+
+function mapOverTracks(tracks){
+  tracks.map(function(track){
+    var list = "<li id=" + track.track.name + " class='playlistItem'>" + track.track.name + ", by " + track.track.artists[0].name + "</li>"
+    document.getElementById('trackList').innerHTML += list;
+  })
+  $('li.playlistItem').click(function() {
+    var access_token = localStorage.getItem("access_token");
+    getPlaylistTracks(access_token);
+  })
 }
