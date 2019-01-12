6a55a4e58d5b2818b84c4ba40ef9eef95a5f7afe
Jon Washington <jonathan.washington@gmail.com>
2019-01-12 13:13:33 -0800

    minor code adjustments including gitignore for node modules

--- public/scripts/last.js
+++ public/scripts/last.js
@@ -12,3 +12,29 @@ function getLastTracks() {
     }
   });
 }
+
+function getLastPlayed() {
+  $.ajax({
+    type:'POST',
+    url: 'http://ws.audioscrobbler.com/2.0/?method=user.getWeeklyTrackChart&api_key=100a45f60fce336c43b1dac55062e23a&artist=Tennis&username=chikinguy&format=json',
+    success: function(response) {
+      console.log(response);
+    },
+    error: function(code, message){
+      console.log('it didn\'t work!');
+    }
+  });
+}
+
+function getTrackForUser() {
+  $.ajax({
+    type:'POST',
+    url: 'http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=100a45f60fce336c43b1dac55062e23a&track=My+Emotions+Are+Blinding&artist=Tennis&username=chikinguy&format=json',
+    success: function(response) {
+      console.log(response);
+    },
+    error: function(code, message){
+      console.log('it didn\'t work!');
+    }
+  });
+}
