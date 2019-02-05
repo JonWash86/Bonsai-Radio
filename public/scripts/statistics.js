9ed94fea03a45f4db1aaf871a3e6bcd8584b44de
Jon Washington <jonathan.washington@gmail.com>
2019-02-05 11:47:05 -0800

    stat and date range sorting is coming together

--- public/scripts/statistics.js
+++ public/scripts/statistics.js
@@ -8,7 +8,7 @@ function fourWeeks(){
 $('#sortByMostPlays').click(function(){
   trackListSorted = "byTop";
   playListTracks.sort(function(obj1, obj2){
-    return obj2.playTracker - obj1.playTracker;
+    return obj2.activeStat.counter - obj1.activeStat.counter;
   });
   $("#trackList").children().remove();
   playListTracks.map(function(track){
@@ -19,7 +19,7 @@ $('#sortByMostPlays').click(function(){
 $('#sortByFewestPlays').click(function(){
   trackListSorted = "byBottom";
   playListTracks.sort(function(obj1, obj2){
-    return obj1.playTracker - obj2.playTracker;
+    return obj1.activeStat.counter - obj2.activeStat.counter;
   });
   $("#trackList").children().remove();
   playListTracks.map(function(track){
@@ -30,16 +30,15 @@ $('#sortByFewestPlays').click(function(){
 // this function manipulates the playcount stat and $('#dateRange') to reflect the previous two weeks.
 $('#twoWeekButton').click(function(){
   currentRange = "twoWeekPlays";
-  if (trackListSorted = "byTop"){
+  if (trackListSorted === "byTop"){
     playListTracks.sort(function(obj1, obj2){
       return obj2.twoWeekPlays - obj1.twoWeekPlays;
-    })
-    $("#trackList").children().remove();
-    playListTracks.map(function(track){
-      track.activeStat.counter = track.twoWeekPlays;
-      track.activeStat.spanText = "two weeks";
-      writePlayListToPanel(track);
-    })
+    });
   }
-
+  $("#trackList").children().remove();
+  playListTracks.map(function(track){
+    track.activeStat.counter = track.twoWeekPlays;
+    track.activeStat.spanText = "two weeks";
+    writePlayListToPanel(track);
+  })
 })
